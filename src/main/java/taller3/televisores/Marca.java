4e471cc261a086a1c5f3e25043d7dc3177b5dbf7
SantiagoOchoaQuintero <sochoaqu@unal.edu.co>
2024-02-23 19:20:20 -0500

    Create Marca.java
    Creamos la clase marca version 1

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,17 @@
+package televisores;
+public class Marca {
+	
+	private String nombre;
+	
+	public Marca(nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+	
+	public setNombre(nombre){
+		this.nombre = nombre;
+	}
+}
