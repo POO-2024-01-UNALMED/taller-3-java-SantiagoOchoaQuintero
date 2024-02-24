73533a681a05d366dddc60ab018b18c5537a8e1e
SantiagoOchoaQuintero <sochoaqu@unal.edu.co>
2024-02-24 15:58:36 -0500

    Update Control.java
    correcion atributos privados

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -5,55 +5,46 @@ public class Control {
 	
 	public void enlazar() {
 		this.tv = tv;
-		tv.control = this.control;
+		//tv.control = this.control;
 	}
 	public void turnOn() {
-		this.tv.estado = true;
+		this.tv.turnOn();
 	}
 	public void turnOff(){
-		this.tv.estado = false;
+		this.tv.turnOff();
 	}
 	public void canalUp() {
-		if(this.tv.estado == True) {
-		if(this.tv.canal==120) {
+		if(this.tv.getEstado() == True) {
+		if(this.tv.getCanal()==120) {
 			;
 		}
 		else {
-			this.tv.canal++;
+			this.tv.canalUp();
 		}
 		}
 
 	}
 	public void canalDown() {
-		if(this.tv.estado == True) {
-		if(this.tv.canal==1) {
+		if(this.tv.getEstado() == True) {
+		if(this.tv.getCanal()==1) {
 			;
 		}else {
-			this.tv.canal--;	
+			this.tv.canalDown();	
 		}
 		}
 	}
 	
 	public void volumenDown() {
-		if(this.tv.estado == True) {
-			if(this.tv.volumen <= 7) {
-				this.tv.volumen--;
-			}
-		}
-		
+		this.tv.volumendDown();
 	}
 	public void voluemUp() {
-		if(this.tv.estado == True) {
-			if(this.tv.volumen >= 0) {
-				this.tv.volumen++;
-			}
-		}
+		this.tv.volumenUp();
 	}
 	public void setCanal(int canal) {
-		this.tv.canal = canal;
+		this.tv.setCanal(canal);
 	}
 	public void setVolumen(int volumen) {
-		this.tv.volumen = volumen;
+		this.tv.setVolumen(volumen);
 	}
 	public void setTv(TV tv) {
 		this.tv = tv;
